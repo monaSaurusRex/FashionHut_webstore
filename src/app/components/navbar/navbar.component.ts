140b4362bd8b275740b20dcd386ec760bb611988
Monique <moniquenagel24.mn@gmail.com>
2023-06-04 18:54:41 +0200

    Added itmes onto navbar and made it mobile reponsive.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { faShoppingCart, faBars, faTimes, faUserAlt, faSearch} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+    cart = faShoppingCart;
+    bars = faBars;
+    close = faTimes;
+    user = faUserAlt;
+    search = faSearch;
+
+    constructor(){}
+
+    ngOnInit() {}
 
 }
