e6be0efe284998c1b736f17642dd605a564c0e1a
Mamokgosi <preciousmaphutha@173@gmail.com>
2023-06-05 12:47:48 +0200

    I have added an image on view all products component and add cart button

--- src/app/pages/view-all-products/view-all-products.component.ts
+++ src/app/pages/view-all-products/view-all-products.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faShoppingCart, faBars, faTimes, faUserAlt, faSearch} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-view-all-products',
   templateUrl: './view-all-products.component.html',
   styleUrls: ['./view-all-products.component.css']
 })
-export class ViewAllProductsComponent {
+export class ViewAllProductsComponent implements OnInit{
+ 
+  cart = faShoppingCart;
+  constructor (){}
 
+ products: any [] = [];
+
+  ngOnInit() {}     
+  
 }
