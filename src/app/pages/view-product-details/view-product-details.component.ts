ba0d013535a6f12dc3fce8f317e5fe4ed764182a
ntokozo9 <ntokozoblessingradebe98@gmail.com>
2023-06-07 14:36:09 +0200

    added functionality to the quantity button

--- src/app/pages/view-product-details/view-product-details.component.ts
+++ src/app/pages/view-product-details/view-product-details.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faArrowLeft, faCartPlus, faMinus, faPlus, faSearch, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-view-product-details',
   templateUrl: './view-product-details.component.html',
   styleUrls: ['./view-product-details.component.css'],
 })
-export class ViewProductDetailsComponent {
+export class ViewProductDetailsComponent implements OnInit{
+  title = 'increment-decrement';
+  productquantity:number=1;
+  constructor(){}
+  ngOnInit(): void {
+      
+  }
+  quantity(value:string){
+    if(this.productquantity < 20 && value == "max" ){
+      this.productquantity +=1;
+    }else if(this.productquantity>1 && value =="min"){
+      this.productquantity -=1;
+    }
+    
+    }
 
+  
 
+  cart = faShoppingCart;
+  addToCart = faCartPlus;
+  increase = faPlus;
+  decrease = faMinus;
+  close = faTimes;
+  back = faArrowLeft;
 
 
 
