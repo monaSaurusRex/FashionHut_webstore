dcac9caa5d60211e55b02af5506ef219e554886b
monaSaurusRex <moniquenagel24.mn@gmail.com>
2023-06-09 18:30:19 +0200

    Updated styles for view details page and changed look of quantity selector to look more like the figma designs.

--- src/app/pages/view-product-details/view-product-details.component.ts
+++ src/app/pages/view-product-details/view-product-details.component.ts
@@ -20,30 +20,29 @@ import { FakestoreService } from 'src/app/services/fakestoreapi/fakestore.servic
   styleUrls: ['./view-product-details.component.css'],
 })
 export class ViewProductDetailsComponent implements OnInit {
-  
-
   title = 'increment-decrement';
-  productquantity: number = 1;
+  productQuantity: number = 1;
 
-  cart = faShoppingCart;
-  addToCartIcon = faCartPlus;
-  increase = faPlus;
-  decrease = faMinus;
-  close = faTimes;
-  back = faArrowLeft;
+  addToCartIcon = faCartPlus;;
+  increaseIcon = faPlus;
+  decreaseIcon = faMinus;
 
   id: any;
   product!: Product;
-  
+
   itemsInCart: number = 0;
   subscription!: Subscription;
-  
 
-  constructor(private _fakeStoreService: FakestoreService, private route: ActivatedRoute, private router: Router, private _cartService:CartService) {}
+  constructor(
+    private _fakeStoreService: FakestoreService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private _cartService: CartService
+  ) {}
 
-  ngOnInit(): void{
-    console.log(this.id = this.route.snapshot.params['id']);
-    this.getProductDetails(this.id = this.route.snapshot.params['id']);
+  ngOnInit(): void {
+    console.log((this.id = this.route.snapshot.params['id']));
+    this.getProductDetails((this.id = this.route.snapshot.params['id']));
 
     this.subscription = this._cartService
       .getTotalItemsInCart()
@@ -52,12 +51,11 @@ export class ViewProductDetailsComponent implements OnInit {
       });
   }
 
-
   quantity(value: string) {
-    if (this.productquantity < 20 && value == 'max') {
-      this.productquantity += 1;
-    } else if (this.productquantity > 1 && value == 'min') {
-      this.productquantity -= 1;
+    if (this.productQuantity < 20 && value == 'increase') {
+      this.productQuantity += 1;
+    } else if (this.productQuantity > 1 && value == 'decrease') {
+      this.productQuantity -= 1;
     }
   }
 
@@ -71,22 +69,19 @@ export class ViewProductDetailsComponent implements OnInit {
   addToCart(product: any) {
     // console.log(product)
     if (product) {
-      if(this.productquantity>=2){ //if the quantity selector is more than the default quantity
-        this.itemsInCart += this.productquantity;
-      }
-      else{
+      if (this.productQuantity >= 2) {
+        //if the quantity selector is more than the default quantity
+        this.itemsInCart += this.productQuantity;
+      } else {
         this.itemsInCart++;
       }
-
     }
 
-    console.log("Items in cart: ", this.itemsInCart);
+    console.log('Items in cart: ', this.itemsInCart);
     let count = {
       totalItems: this.itemsInCart,
     };
     // console.log(this._cartService.getTotalItemsInCart());
     this._cartService.setTotalItemsInCart(count);
   }
-
-  
 }
