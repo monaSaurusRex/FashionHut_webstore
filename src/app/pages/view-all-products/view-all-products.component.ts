b4e7ffb51800e27efb3072ed5e6ffccd0ebc8a4a
monaSaurusRex <moniquenagel24.mn@gmail.com>
2023-06-10 11:52:45 +0200

    Fixed grid layout and padding issues with card and product title, price and card.

--- src/app/pages/view-all-products/view-all-products.component.ts
+++ src/app/pages/view-all-products/view-all-products.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-  faShoppingCart,
-  faBars,
-  faTimes,
-  faUserAlt,
-  faSearch,
-} from '@fortawesome/free-solid-svg-icons';
+import { faCartPlus,} from '@fortawesome/free-solid-svg-icons';
 import { Subscription } from 'rxjs';
 import { Product } from 'src/app/interfaces/product';
 import { CartService } from 'src/app/services/cart/cart.service';
@@ -25,7 +19,7 @@ export class ViewAllProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
 
   //icons
-  cart = faShoppingCart;
+  addToCartIcon = faCartPlus;;
 
   //cart services function variables
   itemsInCart: number = 0;
