b4803e2b5098e5a8108a9c77543cbdf97662fc8b
monaSaurusRex <moniquenagel24.mn@gmail.com>
2023-06-11 19:04:21 +0200

    Added functional searchbar and added basic styling to it.

--- src/app/pages/view-all-products/view-all-products.component.ts
+++ src/app/pages/view-all-products/view-all-products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { faCartPlus,} from '@fortawesome/free-solid-svg-icons';
+import { faCartPlus, faSearch, faTimes,} from '@fortawesome/free-solid-svg-icons';
 import { Subscription } from 'rxjs';
 import { Product } from 'src/app/interfaces/product';
 import { CartService } from 'src/app/services/cart/cart.service';
@@ -19,7 +19,9 @@ export class ViewAllProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
 
   //icons
-  addToCartIcon = faCartPlus;;
+  addToCartIcon = faCartPlus;
+  searchIcon = faSearch;
+  closeIcon = faTimes;
 
   //cart services function variables
   itemsInCart: number = 0;
