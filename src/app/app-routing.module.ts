087ece57b659cf749eec519e33ecc203abc0c05b
Mamokgosi <preciousmaphutha@173@gmail.com>
2023-06-14 15:49:09 +0200

    Added the registration form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewAllProductsComponent } from './pages/view-all-products/view-all-products.component';
 import { ViewProductDetailsComponent } from './pages/view-product-details/view-product-details.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
+  {path: 'register', component: RegisterComponent},
   {path: '', component: ViewAllProductsComponent},
   {path: 'view-product/:id', component: ViewProductDetailsComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ViewAllProductsComponent } from './pages/view-all-products/view-all-products.component';
 import { SearchAndFilterComponent } from './components/search-and-filter/search-and-filter.component';
 import { ViewProductDetailsComponent } from './pages/view-product-details/view-product-details.component';
-
 import { FilterPipe } from './services/filter/filter.pipe';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +20,8 @@ import { FilterPipe } from './services/filter/filter.pipe';
     ViewAllProductsComponent,
     SearchAndFilterComponent,
     FilterPipe,
-    ViewProductDetailsComponent
+    ViewProductDetailsComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
