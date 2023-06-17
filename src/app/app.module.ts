03a34662971d830073ade12136a248afcdb51522
monaSaurusRex <moniquenagel24.mn@gmail.com>
2023-06-17 14:00:18 +0200

    Added cart list component and cart page component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+// MODULES
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,11 +8,17 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+// COMPONENTS
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ViewAllProductsComponent } from './pages/view-all-products/view-all-products.component';
 import { SearchAndFilterComponent } from './components/search-and-filter/search-and-filter.component';
+import { CartItemListComponent } from './components/cart-items-list/cart-item-list/cart-item-list.component';
+// PAGES
+import { ViewAllProductsComponent } from './pages/view-all-products/view-all-products.component';
 import { ViewProductDetailsComponent } from './pages/view-product-details/view-product-details.component';
+import { CartComponent } from './pages/cart/cart.component';
+// SERVICES
 
+// PIPES
 import { FilterPipe } from './services/filter/filter.pipe';
 
 @NgModule({
@@ -21,7 +28,9 @@ import { FilterPipe } from './services/filter/filter.pipe';
     ViewAllProductsComponent,
     SearchAndFilterComponent,
     FilterPipe,
-    ViewProductDetailsComponent
+    ViewProductDetailsComponent,
+    CartItemListComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/cart-items-list/cart-item-list/cart-item-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-item-list',
+  templateUrl: './cart-item-list.component.html',
+  styleUrls: ['./cart-item-list.component.css']
+})
+export class CartItemListComponent {
+
+}
