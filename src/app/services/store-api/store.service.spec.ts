877e71f813c01700703d5de8e1671e4b23bb37db
monaSaurusRex <moniquenagel24.mn@gmail.com>
2023-06-17 16:45:10 +0200

    Renamed fakestore api to store api for future integration of real ecommerce api.

--- src/app/services/store-api/store.service.spec.ts
+++ src/app/services/store-api/store.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { FakestoreService } from './fakestore.service';
+import { FakestoreService } from './store.service';
 
 describe('FakestoreService', () => {
   let service: FakestoreService;
