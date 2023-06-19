49463d936dec415367968f9f0d82ac49ac3352df
Mamokgosi <preciousmaphutha@173@gmail.com>
2023-06-19 11:18:36 +0200

    Validates all the field inputs are filled

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from '../helper/validateform';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+}
+else {
+  //console.log("form is not valid");
 
+  ValidateForm.validateAllFormFields(this.registerForm);
+  //alert("Form is not valid");
+}
+}
+  type: string = "password";
+  registerForm!: FormGroup;
+
+  constructor(private formbuilder: FormBuilder){
+
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formbuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      password: ['', Validators.required],
+      confirm:['', Validators.required]
+  })
+}
 }
