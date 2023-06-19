64812eede8b4c0869e36b6878370f258c0f2ad48
monaSaurusRex <moniquenagel24.mn@gmail.com>
2023-06-19 04:38:45 +0200

    Created checkout page and added routingto it with button that will be visible on off canvas cart sidebar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewAllProductsComponent } from './pages/view-all-products/view-all-products.component';
 import { ViewProductDetailsComponent } from './pages/view-product-details/view-product-details.component';
+import { CheckoutComponent } from './pages/checkout/checkout/checkout.component';
 
 const routes: Routes = [
   {path: '', component: ViewAllProductsComponent},
   {path: 'view-product/:id', component: ViewProductDetailsComponent},
+  {path: 'checkout', component: CheckoutComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
  
 ];
--- src/app/services/store-api/store.service.spec.ts
+++ src/app/services/store-api/store.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { FakestoreService } from './store.service';
+import { StoreService } from './store.service';
 
-describe('FakestoreService', () => {
-  let service: FakestoreService;
+describe('StoreService', () => {
+  let service: StoreService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(FakestoreService);
+    service = TestBed.inject(StoreService);
   });
 
   it('should be created', () => {
