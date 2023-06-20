3715175342f33d401195438a51810b5deea56d55
ntokozo9 <ntokozoblessingradebe98@gmail.com>
2023-06-20 16:28:59 +0200

    added validations to the payment form

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -7,23 +7,53 @@ import { Validators } from '@angular/forms';
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
-  contactForm!: FormGroup;
+export class CheckoutComponent {
+  cardNameControl: FormControl = new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]);
+  cardNumberControl: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[0-9]{16}$')
+    
+  ]);
+  cardExpirationControl: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^((0[1-9])|(1[0-2]))\/(\d{2})$')
+  ]);
+  securityCodeControl: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[0-9]{3}$')
+  ]);
 
-  constructor(private formBuilder: FormBuilder) { }
+  suburbCityControl: FormControl = new FormControl('', Validators.required);
+  provinceControl: FormControl = new FormControl('', Validators.required);
+  countryControl: FormControl = new FormControl('', Validators.required);
+  postalCodeControl: FormControl = new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]);
 
-  ngOnInit() {
-    this.contactForm = this.formBuilder.group({
-      ContactForm: this.formBuilder.group({
-        firstName: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        street: ['', Validators.required],
-        city: ['', Validators.required],
-        zip: ['', Validators.required],
-        credit: ['', Validators.required],
-        expiry: ['', Validators.required],
-        cvv: ['', Validators.required]
-      })
-    });
+  get showErrorMessage() {
+    return this.cardExpirationControl.invalid && this.cardExpirationControl.touched;
   }
-}
+
+  limitCharacterCount(event: any) {
+    const input = event.target as HTMLInputElement;
+    const maxLength = 16;
+
+    if (input.value.length > maxLength) {
+      input.value = input.value.slice(0, maxLength);
+      this.cardNumberControl.setValue(input.value);
+    }
+    
+  }
+  limitSecurityCode(event: any) {
+    const input = event.target as HTMLInputElement;
+    const maxLength = 3;
+
+    if (input.value.length > maxLength) {
+      input.value = input.value.slice(0, maxLength);
+      this.securityCodeControl.setValue(input.value);
+
+    }
+
+    this.securityCodeControl.setValue(input.value);
+  }
+    }
+  
+  
