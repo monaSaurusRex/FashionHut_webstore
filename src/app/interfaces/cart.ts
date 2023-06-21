72c324cd0afe3fb076c5a6baf50076d0a2140740
Monique <monqiquenagel24.mn@gmail.com>
2023-06-21 07:36:50 +0200

    Modified cart model to have more attributes that more closely fits the backen cart model.

--- src/app/interfaces/cart.ts
+++ src/app/interfaces/cart.ts
@@ -5,11 +5,14 @@ export interface Cart {
     id: number;
     items: Item[];
     cartTotal: number; //subtotal for items in cart
+    // customer id
 }
 
 export interface Item{
     id: number;
     // product: Product[];
-    productId: number; // refers to the product added to the cart -> 
-    quantity: number; //quantity of products added to the cart
+    productId: number; // refers to the product added to the cart
+    productName: string; // refers to the name of the product when it was added to the cart
+    quantity: number; //quantity of a product added to the cart
+    price: number; //price of the product when it was added to the cart
 }
