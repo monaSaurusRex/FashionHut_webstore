a93c470deecd3d31c3b435c45395e98980a830d1
Monique <monqiquenagel24.mn@gmail.com>
2023-06-21 14:38:44 +0200

    Added function for subtotal component and made it display more like figma designs.

--- src/app/components/subtotal/subtotal.component.ts
+++ src/app/components/subtotal/subtotal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs/internal/Observable';
+import { CartService } from 'src/app/services/cart/cart.service';
 
 @Component({
   selector: 'app-subtotal',
@@ -11,9 +12,9 @@ export class SubtotalComponent implements OnInit {
 
   subTotal$: Observable<number> | undefined;
 
-  constructor(){
-    
-
+  constructor(private _cartService: CartService){
+    this.subTotal$ = this._cartService.getSubtotal();
+    console.log(this.subTotal$)
   }
 
   ngOnInit(){
