148b277d041e7a1bc5ccfd0375e82767d675adeb
Kamogelo0404 <kamogelonkadimeng04042gmail.com>
2023-06-21 09:06:27 +0200

    user services and functionality for login added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,21 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import ValidateForm from '../helper/validateform';
+import { UserService } from 'src/app/services/user/user.service';
+//import { Router } from 'express-serve-static-core';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  loginForm: FormGroup;
 
-  loginForm!: FormGroup;
-
-  constructor(private fb: FormBuilder){
-
-  }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder, private _userService: UserService, /*private router: Router)*/) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/)]]
@@ -24,11 +22,25 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
-      // Proceed with login logic
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
+  
+      // Call the login method of the AuthService to perform authentication
+      this._userService.login(email, password).subscribe(
+        (response) => {
+          // Login successful, redirect to the checkout page
+          //this.router.navigate(['/checkout']);
+        },
+        (error) => {
+          // Login failed, handle the error
+          console.log('Login failed', error);
+        }
+      );
     } else {
       ValidateForm.validateAllFormFields(this.loginForm);
       // Handle form validation errors
     }
   }
+  
+  
 }
---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private url: string = "https://fakestoreapi.com/"
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    const body = {
+      username: " mor_2314",
+      password: password
+    };
+    return this.http.post<any>(`${this.url}auth/login`, body);
+  }
+  
+}
